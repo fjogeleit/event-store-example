07ad90f76593f6f111f376c748ddc8b9c416603d
Frank Jogeleit <frank.jogeleit@web.de>
2020-09-03 10:14:35 +0200

    update dependencies

--- src/todo/module/todo.controller.ts
+++ src/todo/module/todo.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, HttpException, HttpStatus, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { TodoRepository } from '../model/todo-repository';
 import { TodoBody, TodoService } from './todo.service';
 
